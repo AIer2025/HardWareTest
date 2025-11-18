ff41d2cee34cc71cb69353efbeee9cedfc1c477f
Bight.STONE <littlekoi@sina.com>
2025-11-18 16:36:45 +0800

    修改，输出图像

--- src/LabTestPlatform.Data/Repositories/TestDataRepository.cs
+++ src/LabTestPlatform.Data/Repositories/TestDataRepository.cs
@@ -18,7 +18,26 @@ public class TestDataRepository : ITestDataRepository
     public async Task<IEnumerable<TestDataEntity>> GetByModuleIdAsync(int moduleId)
     {
         using var conn = await _connectionFactory.CreateConnectionAsync();
-        var sql = "SELECT * FROM tb_test_data WHERE module_id = @ModuleId ORDER BY test_time DESC";
+        var sql = @"
+            SELECT 
+                test_id AS TestId,
+                module_id AS ModuleId,
+                batch_id AS BatchId,
+                test_time AS TestTime,
+                test_value AS TestValue,
+                test_unit AS TestUnit,
+                test_type AS TestType,
+                failure_time AS FailureTime,
+                failure_mode AS FailureMode,
+                is_censored AS IsCensored,
+                temperature AS Temperature,
+                humidity AS Humidity,
+                operator AS Operator,
+                remarks AS Remarks,
+                create_time AS CreateTime
+            FROM tb_test_data 
+            WHERE module_id = @ModuleId 
+            ORDER BY test_time DESC";
         return await conn.QueryAsync<TestDataEntity>(sql, new { ModuleId = moduleId });
     }
 
@@ -26,9 +45,26 @@ public class TestDataRepository : ITestDataRepository
     {
         using var conn = await _connectionFactory.CreateConnectionAsync();
         var sql = @"
-            SELECT * FROM tb_test_data 
-            WHERE module_id = @ModuleId AND test_type = @TestType
-            AND failure_time IS NOT NULL
+            SELECT 
+                test_id AS TestId,
+                module_id AS ModuleId,
+                batch_id AS BatchId,
+                test_time AS TestTime,
+                test_value AS TestValue,
+                test_unit AS TestUnit,
+                test_type AS TestType,
+                failure_time AS FailureTime,
+                failure_mode AS FailureMode,
+                is_censored AS IsCensored,
+                temperature AS Temperature,
+                humidity AS Humidity,
+                operator AS Operator,
+                remarks AS Remarks,
+                create_time AS CreateTime
+            FROM tb_test_data 
+            WHERE module_id = @ModuleId 
+              AND test_type = @TestType
+              AND failure_time IS NOT NULL
             ORDER BY failure_time ASC";
         return await conn.QueryAsync<TestDataEntity>(sql, new { ModuleId = moduleId, TestType = testType });
     }
@@ -38,11 +74,11 @@ public class TestDataRepository : ITestDataRepository
         using var conn = await _connectionFactory.CreateConnectionAsync();
         var sql = @"
             INSERT INTO tb_test_data (
-                module_id, test_time, test_value, test_unit, test_type,
+                module_id, batch_id, test_time, test_value, test_unit, test_type,
                 failure_time, failure_mode, is_censored, temperature,
                 humidity, operator, remarks
             ) VALUES (
-                @ModuleId, @TestTime, @TestValue, @TestUnit, @TestType,
+                @ModuleId, @BatchId, @TestTime, @TestValue, @TestUnit, @TestType,
                 @FailureTime, @FailureMode, @IsCensored, @Temperature,
                 @Humidity, @Operator, @Remarks
             );
@@ -55,11 +91,11 @@ public class TestDataRepository : ITestDataRepository
         using var conn = await _connectionFactory.CreateConnectionAsync();
         var sql = @"
             INSERT INTO tb_test_data (
-                module_id, test_time, test_value, test_unit, test_type,
+                module_id, batch_id, test_time, test_value, test_unit, test_type,
                 failure_time, failure_mode, is_censored, temperature,
                 humidity, operator, remarks
             ) VALUES (
-                @ModuleId, @TestTime, @TestValue, @TestUnit, @TestType,
+                @ModuleId, @BatchId, @TestTime, @TestValue, @TestUnit, @TestType,
                 @FailureTime, @FailureMode, @IsCensored, @Temperature,
                 @Humidity, @Operator, @Remarks
             )";
